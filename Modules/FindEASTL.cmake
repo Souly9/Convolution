7f33f2b86f924c94b964292321eefec3b4a6342e
Souly9 <kai.eschmann@gmx.de>
2024-08-22 22:03:34 +0900

    Rework CMakeLists to download dependancies

--- Modules/FindEASTL.cmake
+++ Modules/FindEASTL.cmake
@@ -1,16 +1,3 @@
-set(EASTL_ROOT_DIR ${CMAKE_SOURCE_DIR}/External/EASTL)
-include_directories (${EASTL_ROOT_DIR}/include)
-include_directories (${EASTL_ROOT_DIR}/test/packages/EAAssert/include)
-include_directories (${EASTL_ROOT_DIR}/test/packages/EABase/include/Common)
-include_directories (${EASTL_ROOT_DIR}/test/packages/EAMain/include)
-include_directories (${EASTL_ROOT_DIR}/test/packages/EAStdC/include)
-include_directories (${EASTL_ROOT_DIR}/test/packages/EATest/include)
-include_directories (${EASTL_ROOT_DIR}/test/packages/EAThread/include)
-
-find_library(EASTL_LIBRARY NAMES EASTL PATHS ${EASTL_ROOT_DIR}/out/build/x64-Debug/ REQUIRED)
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(EASTL
-  REQUIRED_VARS
-  EASTL_LIBRARY
-)
-add_custom_target(NatVis SOURCES ${EASTL_ROOT_DIR}/doc/EASTL.natvis)
+set(EASTL_LIBRARY debug ${EASTL_ROOT_DIR}/out/build/x64-Clang-Debug/EASTL.lib optimized ${EASTL_ROOT_DIR}/out/build/x64-Clang-Release/EASTL.lib)
+add_custom_target(NatVis SOURCES  ${CMAKE_SOURCE_DIR}/External/EASTL/doc/EASTL.natvis)
+set(EASTL_ROOT_DIR ${CMAKE_SOURCE_DIR}/External/EASTL/include)
